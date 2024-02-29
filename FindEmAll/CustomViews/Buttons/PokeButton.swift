13b5f535fcda89c1e5bf3d4049c79714c0bdbedf
7과11사이 <dongjunlee222@gmail.com>
2024-02-29 23:36:44 +0900

    [ADD]: custom Button

--- FindEmAll/CustomViews/Buttons/PokeButton.swift
+++ FindEmAll/CustomViews/Buttons/PokeButton.swift
@@ -8,13 +8,35 @@
 import UIKit
 
 class PokeButton: UIButton {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
     }
-    */
-
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    convenience init(color: UIColor, image: String) {
+        self.init(frame: .zero)
+        set(color: color, withImage: image)
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        layer.cornerRadius = 50
+        
+        NSLayoutConstraint.activate([
+            heightAnchor.constraint(equalToConstant: 100),
+            widthAnchor.constraint(equalToConstant: 100)
+        ])
+    }
+    
+    private func set(color: UIColor, withImage image: String) {
+        configuration?.image = UIImage(systemName: image)
+        configuration?.imagePadding = 5
+        configuration?.imagePlacement = .all
+        
+        backgroundColor = color
+    }
 }
