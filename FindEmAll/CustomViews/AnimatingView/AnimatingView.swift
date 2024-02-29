5950f152e5e9d9428cf353e80a023615ce36b749
7과11사이 <dongjunlee222@gmail.com>
2024-02-29 23:36:05 +0900

    [ADD]: AnimatingView

--- FindEmAll/CustomViews/AnimatingView/AnimatingView.swift
+++ FindEmAll/CustomViews/AnimatingView/AnimatingView.swift
@@ -6,15 +6,41 @@
 //
 
 import UIKit
+enum Movement {
+    case up, down
+}
 
 class AnimatingView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
     }
-    */
-
+    
+    convenience init(color: UIColor) {
+        self.init(frame: .zero)
+        backgroundColor = color
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 30
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func move(to position: Movement, completion: (() -> Void)?) {
+        switch position {
+        case .up:
+            UIView.animate(withDuration: 0.5) {
+                self.frame.origin.y -= 300
+            } completion: { _ in completion?() }
+            
+        case .down:
+            UIView.animate(withDuration: 0.5) {
+                self.frame.origin.y += 300
+            } completion: { _ in completion?() }
+        }
+    }
 }
