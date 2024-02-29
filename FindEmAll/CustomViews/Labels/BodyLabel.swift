6f0f9e58a13c793859bc0574aa05a053b549717d
7과11사이 <dongjunlee222@gmail.com>
2024-02-29 23:36:31 +0900

    [ADD]: custom labels

--- FindEmAll/CustomViews/Labels/BodyLabel.swift
+++ FindEmAll/CustomViews/Labels/BodyLabel.swift
@@ -8,13 +8,18 @@
 import UIKit
 
 class BodyLabel: UILabel {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        adjustsFontSizeToFitWidth = true
+        textColor = .secondaryLabel
+    }
 }
--- FindEmAll/CustomViews/Labels/TitleLabel.swift
+++ FindEmAll/CustomViews/Labels/TitleLabel.swift
@@ -8,13 +8,25 @@
 import UIKit
 
 class TitleLabel: UILabel {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    convenience init(text: String, textAlignment: NSTextAlignment, fontSize: CGFloat) {
+        self.init(frame: .zero)
+        self.text = text
+        self.textAlignment = textAlignment
+        self.font = UIFont.systemFont(ofSize: fontSize, weight: .heavy)
+    }
+    
+    private func configure() {
+        textColor = .systemBackground
+        adjustsFontForContentSizeCategory = true
+        translatesAutoresizingMaskIntoConstraints = false
+    }
 }
