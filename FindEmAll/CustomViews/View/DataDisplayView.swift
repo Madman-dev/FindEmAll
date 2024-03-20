3e09ccdbec2ce41c80a5f7258dae08027e0258c8
7과11사이 <dongjunlee222@gmail.com>
2024-03-20 23:04:44 +0900

    [FEAT]: ⚙️ Header DataDisplayView

--- FindEmAll/CustomViews/View/DataDisplayView.swift
+++ FindEmAll/CustomViews/View/DataDisplayView.swift
@@ -8,13 +8,38 @@
 import UIKit
 
 class DataDisplayView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    let titleLabel = TitleLabel(textAlignment: .center, fontSize: 25)
+    let dataLabel = BodyLabel(textAlignment: .center)
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        configure()
+        configureLabels()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        backgroundColor = .white
+        layer.cornerRadius = 10
+        
+        titleLabel.text = "제목"
+        dataLabel.text = "테스트"
+    }
+    
+    private func configureLabels() {
+        addSubviews(titleLabel, dataLabel)
+        
+        NSLayoutConstraint.activate([
+            
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 40),
+            titleLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            
+            dataLabel.leadingAnchor.constraint(equalTo: titleLabel.trailingAnchor, constant: 10),
+            dataLabel.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
 }
