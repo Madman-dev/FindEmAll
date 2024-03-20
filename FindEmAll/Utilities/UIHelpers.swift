1220d34e05e6ae33908fce9ef42d5250a54a29cb
7과11사이 <dongjunlee222@gmail.com>
2024-03-20 23:00:32 +0900

    [FEAT]: ⚙️ Custom Collectionview FlowLayout

--- FindEmAll/Utilities/UIHelpers.swift
+++ FindEmAll/Utilities/UIHelpers.swift
@@ -5,4 +5,21 @@
 //  Created by Porori on 3/20/24.
 //
 
-import Foundation
+import UIKit
+
+enum UIHelpers {
+    // Learn again how the cell's size is modified
+    // 10
+    static func createThreeColumnFlowlayout(in view: UIView) -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 8
+        let availableWidth = width - (padding * 2)
+        let itemWidth = availableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth)
+        
+        return flowLayout
+    }
+}
