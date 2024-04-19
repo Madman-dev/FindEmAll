a612ab4743bbc49f180e579efe98cb5f879f55cf
7과11사이 <dongjunlee222@gmail.com>
2024-04-19 22:43:37 +0900

    FIN-26 feat ✨: AlertContainer added

--- FindEmAll/CustomViews/View/AlertContainerView.swift
+++ FindEmAll/CustomViews/View/AlertContainerView.swift
@@ -7,14 +7,22 @@
 
 import UIKit
 
+// PokeAlert container
 class AlertContainerView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        backgroundColor = .blue
+        layer.cornerRadius = 15
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.white.cgColor
+        translatesAutoresizingMaskIntoConstraints = false
+    }
 }
--- FindEmAll/CustomViews/ViewControllers/PokeAlertVC.swift
+++ FindEmAll/CustomViews/ViewControllers/PokeAlertVC.swift
@@ -8,22 +8,81 @@
 import UIKit
 
 class PokeAlertVC: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    private let containerView = AlertContainerView()
+    private let titleLabel = TitleLabel(textAlignment: .center, fontSize: 25)
+    private let messageLabel = BodyLabel(textAlignment: .left)
+    private let actionName = PokeButton(color: .black)
+    
+    private var alertTitle: String?
+    private var alertMessage: String?
+    private var buttonTitle: String?
+    
+    init(title: String, message: String, buttonTitle: String) {
+        super.init(nibName: nil, bundle: nil)
+        self.alertTitle = title
+        self.alertMessage = message
+        self.buttonTitle = buttonTitle
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureContainer()
+        configureTitle()
+        configureMessage()
+        configureButton()
+    }
+    
+    private func configureContainer() {
+        view.backgroundColor = .black.withAlphaComponent(0.5)
+        view.addSubviews(containerView, titleLabel, messageLabel, actionName)
+        
+        NSLayoutConstraint.activate([
+            containerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            containerView.heightAnchor.constraint(equalToConstant: 300),
+            containerView.widthAnchor.constraint(equalToConstant: 300)
+        ])
+    }
+    
+    private func configureTitle() {
+        titleLabel.text = alertTitle ?? "Error Message"
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 20),
+            titleLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20),
+            titleLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20),
+            titleLabel.heightAnchor.constraint(equalToConstant: 80)
+        ])
+    }
+    
+    private func configureMessage() {
+        messageLabel.text = alertMessage ?? "No... Seriously...😢"
+        
+        NSLayoutConstraint.activate([
+            messageLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 5),
+            messageLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20),
+            messageLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20),
+            messageLabel.heightAnchor.constraint(equalToConstant: 80)
+        ])
+    }
+    
+    private func configureButton() {
+        actionName.setTitle(buttonTitle ?? "Pass!", for: .normal)
+        
+        // button is dismissed when tapped
+        let action = UIAction { [weak self] action in self?.dismiss(animated: true) }
+        actionName.addAction(action, for: .touchUpInside)
+        
+        NSLayoutConstraint.activate([
+            actionName.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+            actionName.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -10),
+            actionName.heightAnchor.constraint(equalToConstant: 50),
+            actionName.widthAnchor.constraint(equalToConstant: 80)
+        ])
+    }
 }
